7d7829fd593b4790fdbfd806eaa03fddbc99bdeb
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-03-10 17:12:17 +0200

    Добавил класс с URL

---
+++ src/test/java/eDiscovery/helpers/RelativeURL.java
@@ -0,0 +1,10 @@
+package eDiscovery.helpers;
+
+public class RelativeURL {
+
+    public static final String URL_SEARCH_QUERY_LIST        = "/search-query/list";
+    public static final String URL_METADATA_FILTERS_LIST    = "/metadata-filters/list";
+    public static final String URL_SEARCH_PLACE_GROUP_LIST  = "/search-places/groups/list";
+    public static final String URL_SEARCH_PLACES_LIST       = "/search-places/list";
+
+}
